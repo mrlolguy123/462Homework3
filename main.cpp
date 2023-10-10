5b34fc7c9203018644adec0e99e521f25a8235b8
mrlolguy123 <24dannle@gmail.com>
2023-10-10 16:12:44 -0400

    Finished implementing This doesn't work

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 #include <string>
 #include <stack>
 
-bool isOp(char c)
+bool isOperator(char c)
 {
     return ((c=='+') || (c=='-') || (c=='*') || (c=='/'));
 }
 
-int precedence(char c)
+bool isOperand(char c)
+{
+    return (c <= '9' && c >= '0');
+}
+
+int prdnc(char c)
 {
     switch(c)
     {
@@ -25,25 +30,49 @@ int precedence(char c)
 std::string inToPost(std::string str)
 {
     std::string post = "";
-    std::stack<char> operatorStack;
+    std::stack<char> opStack;
 
     for(int x = 0; x<str.length(); x++)
     {
-        if(isOp(str.at(x))) { operatorStack.push(str.at(x)); }
-        else
-            post = post + str.at(x);
+        std::cout << x << ". Post = " << post << std::endl;
+
+        if(str.at(x) == ' ') {
+            continue; // skip whitespace
+        }
+
+        if(isOperand(str.at(x))) {
+            post = post + str.at(x); // if operand, add to post
+        }
+
+        else if(str.at(x) == '(') {
+            opStack.push(str.at(x)); // if (, push to stack
+        }
+
+        else if(str.at(x) == ')') { // if ), pop all in stack
+            while(opStack.top() != '(') { post = post + opStack.top(); opStack.pop(); } // all pops go to post exp
+            opStack.pop(); // get rid of last '('
+        }
+
+        else if(isOperator(str.at(x))) {
+            while(prdnc(str.at(x)) <= prdnc(opStack.top()))
+                post = post + opStack.top(); opStack.pop();
+            opStack.push(str.at(x));
+        }
+
     }
 
-    while(!operatorStack.empty())
+    while(opStack.size() != 0)
     {
-        post = post + operatorStack.top();
-        operatorStack.pop();
+        post = post + opStack.top();
+        opStack.pop();
     }
 
+    std::cout << "Done" << std::endl;
     return post;
 }
 
 int main() {
     std::string infix = "5+9-2+1";
+    std::cout << "Length of infix: " << infix.length() << std::endl;
     std::cout << inToPost(infix);
 }
